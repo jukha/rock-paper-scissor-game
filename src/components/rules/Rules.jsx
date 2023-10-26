80b60b1ffb088dcc3e46e969cb46862e0c49fc24
jukha <34809502+jukha@users.noreply.github.com>
2023-10-27 04:13:35 +0500

    Close modal when Esc. is pressed

--- src/components/rules/Rules.jsx
+++ src/components/rules/Rules.jsx
@@ -1,26 +1,40 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function Rules() {
   const [showModal, setShowModal] = useState(false);
-  function openModal() {
-    setShowModal(true);
+  function toggleModal() {
+    setShowModal((state) => !state);
   }
-  function closeModal() {
-    setShowModal(false);
-  }
+  useEffect(() => {
+    function handleKeyDown(event) {
+      if (event.key === "Escape") {
+        toggleModal();
+      }
+    }
+
+    if (showModal) {
+      document.addEventListener("keydown", handleKeyDown);
+    } else {
+      document.removeEventListener("keydown", handleKeyDown);
+    }
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [showModal]);
   return (
     <>
-      <a className="flex justify-end" onClick={openModal}>
-        <button className="uppercase font-semibold py-2 px-8 text-white border border-white rounded-md">
+      <a className="flex justify-end" onClick={toggleModal}>
+        <button className="rounded-md border border-white px-8 py-2 font-semibold uppercase text-white">
           Rules
         </button>
       </a>
       {showModal && (
-        <section className="fixed inset-0 z-30 flex justify-center items-center bg-black bg-opacity-50">
-          <div className="bg-white p-6 rounded-md">
-            <div className="flex justify-between items-center mb-7">
-              <h3 className="uppercase text-dark text-2xl font-bold">Rules</h3>
-              <button onClick={closeModal}>
+        <section className="fixed inset-0 z-30 flex items-center justify-center bg-black bg-opacity-50">
+          <div className="rounded-md bg-white p-6">
+            <div className="mb-7 flex items-center justify-between">
+              <h3 className="text-2xl font-bold uppercase text-dark">Rules</h3>
+              <button onClick={toggleModal}>
                 <img src="/assets/icon-close.svg" />
               </button>
             </div>
